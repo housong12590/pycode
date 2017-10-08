cde045e6c1b9695d46a3a01bb8539b263927b491
hous <304536797@qq.com>
2017-10-08 23:34:16 +0800

    123

--- crawler/window.py
+++ crawler/window.py
@@ -4,10 +4,17 @@ from tkinter import *
 class Window(object):
     def __init__(self):
         self.tk = Tk()
+        self.tk.geometry('800x600+500+250')
         self.tk.title("数据采集")
-        Entry(self.tk, text=" input your text here").pack()
-        self.btn_search = Button(self.tk, bd=2, text=' 搜索', width='6', height="1")
-        self.btn_search.pack()
+        entry_text = StringVar()
+        Entry(self.tk, textvariable=entry_text).grid(sticky="W")  # 单行文本输入
+        entry_text.set("请输入搜索关键字")
+
+        btn_text = StringVar()
+        self.btn_search = Button(self.tk, bd=2, command=setSearchClickListener, textvariable=btn_text, width='6',
+                                 height="1")
+        btn_text.set("搜索")
+        self.btn_search.grid(row=0, column=1)
 
     def search(self):
         print("search...")
@@ -17,3 +24,6 @@ class Window(object):
 
     def show(self):
         self.tk.mainloop()
+
+def setSearchClickListener():
+    print("asdfasdf")
